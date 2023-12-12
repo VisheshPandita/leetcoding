33b0baa26b1484b86db714437748109d749be0bc
vishesh <vishesh.pndta@gmail.com>
2023-12-13 00:08:18 +0530

    leetcoding

---
+++ src/main/java/org/example/problems/P20.java
@@ -0,0 +1,22 @@
+package org.example.problems;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class P20 {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (char c : s.toCharArray()) {
+            if (c == '(')
+                stack.push(')');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c)
+                return false;
+        }
+        return stack.isEmpty();
+    }
+}
