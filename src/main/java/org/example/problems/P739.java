6370900d46f13c18ed5fe3204a8578d9aa61539d
vishesh <vishesh.pndta@gmail.com>
2023-12-13 16:57:32 +0530

    leetcoding

---
+++ src/main/java/org/example/problems/P739.java
@@ -0,0 +1,20 @@
+package org.example.problems;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class P739 {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> stack = new Stack<>();
+        int[] ret = new int[temperatures.length];
+        for(int i = 0; i < temperatures.length; i++) {
+            while(!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int idx = stack.pop();
+                ret[idx] = i - idx;
+            }
+            stack.push(i);
+        }
+        return ret;
+    }
+}
