38c2c6cb933ecd88d9037c9c531965c314524ba0
vishesh <vishesh.pndta@gmail.com>
2024-01-03 23:32:57 +0530

    leetcoding

--- src/main/java/org/example/problems/P167.java
+++ src/main/java/org/example/problems/P167.java
@@ -3,7 +3,7 @@ package org.example.problems;
 public class P167 {
     public int[] twoSum(int[] numbers, int target) {
         int[] ans = new int[2];
-        int ptr1=0,ptr2=numbers.length-1;
+        int ptr1=0,ptr2 = numbers.length-1;
         while(ptr2>ptr1){
             if(numbers[ptr1]+numbers[ptr2]==target){
                 ans[0] = ptr1+1;
